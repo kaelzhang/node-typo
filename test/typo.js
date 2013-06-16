65e23022da9126543ab4e55dc805322dab2f875b
Kael <i@kael.me>
2013-06-17 01:51:39 +0800

    update package.json: description and keywords

--- test/typo.js
+++ test/typo.js
@@ -8,11 +8,11 @@ typo.log(a, {a: 'bold'}, function(err, value) {
     console.log(err, value);
 });
 
-typo.log('{{a}}{{b}}{{c.a}}{{c.b}}', {a: 1, b:2, c: {a: 3}}, function(err, value) {
+typo.log('{{italic a}}{{b}}{{c.a}}{{c.b}}', {a: 1, b:2, c: {a: 3}}, function(err, value) {
     console.log(err, value);
 });
 
-typo.log('{{a}}', {a: 1}, function(err, value) {
+typo.log('{{underline a}}', {a: 1}, function(err, value) {
     console.log(err, value);
 });
 
