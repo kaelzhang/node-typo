987e7a8d5e2e703c04c0718f9eb1e39c67ec3887
Kael <i@kael.me>
2013-06-17 00:43:15 +0800

    explode helper.register to `typo`

--- test/typo.js
+++ test/typo.js
@@ -15,3 +15,12 @@ typo.log('{{a}}{{b}}{{c.a}}{{c.b}}', {a: 1, b:2, c: {a: 3}}, function(err, value
 typo.log('{{a}}', {a: 1}, function(err, value) {
     console.log(err, value);
 });
+
+typo.register('sum', function(value){{
+    return (Array.isArray(value) ? value : [value])
+        .reduce(function(prev, current){
+            return (parseInt(current) || 0) + prev;
+        }, 0);
+}});
+
+typo.log('{{sum 1,2,3}}');      // print 6
