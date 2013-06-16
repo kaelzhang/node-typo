801f3d6e70a0925ff31f6897a8e259f8a9b1ca74
Kael <i@kael.me>
2013-06-17 00:17:06 +0800

    fix a bug that if no helper specified, there will be no stdout; fix callback undefined

--- test/typo.js
+++ test/typo.js
@@ -7,3 +7,11 @@ var a = 'abc{{blue|bold blu e}}{{blue blue}} eee{{bold a}} {{0}}abc{{red red}}';
 typo.log(a, {a: 'bold'}, function(err, value) {
     console.log(err, value);
 });
+
+typo.log('{{a}}{{b}}{{c.a}}{{c.b}}', {a: 1, b:2, c: {a: 3}}, function(err, value) {
+    console.log(err, value);
+});
+
+typo.log('{{a}}', {a: 1}, function(err, value) {
+    console.log(err, value);
+});
