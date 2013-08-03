e28a13666eb9c3f9579d86d51f6e9443cdbb910f
Kael <i@kael.me>
2013-08-03 14:20:37 +0800

    remove helper `rgb` out of typo

--- index.js
+++ index.js
@@ -11,13 +11,6 @@ function typo(options){
 
 typo.Typo = Typo;
 
-
-// util method of RGB
-typo.util = {
-    RGB: require('./lib/util/rgb')
-};
-
-
 // register a global helper or global helpers
 typo.register = function (pattern, parser) {
     Typo._register(Typo.Helpers.prototype, pattern, parser);  
